b94e5e391d83617b60b980d289ab37b0636a1362
Gonzalo Pinat <gonzalo.pinat@gmail.com>
2018-05-31 17:17:24 -0300

    Avances sobre commons

---
+++ Scrapper/Commons/CustomHttpManager/Hacks/HackSucuri.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Commons.CustomHttpManager.Hacks
+{
+    public class HackSucuri
+    {
+        public static bool checkSucuriProtection (string response)
+        {
+            return true;
+        }
+
+        public static bool addSucuriHeaders (string jsCrypt, ref HttpHeaders headers, ref string error)
+        {
+            try
+            {
+                Regex reg = new Regex("S='.+?'", RegexOptions.Singleline);
+
+                string lin0 = string.Empty;
+                string lin1 = string.Empty;
+
+                if (reg.IsMatch(jsCrypt))
+                {
+                    string base64   = reg.Match(jsCrypt).Value.Split("'".ToCharArray()[0])[1];
+                    string js       = Encoding.UTF8.GetString(Convert.FromBase64String(base64));
+                    string[] lines  = getLines(js);
+
+                    string var0 = lines[0].Split('=')[0];
+
+                    string[] exps0 = lines[0].Substring(lines[0].IndexOf("=") + 1).Split('+');
+                    string[] exps1 = lines[1].Substring(lines[1].IndexOf("=") + 1).Split('+');
+
+                    foreach (string exp in exps0)
+                        lin0 += evalExp(exp.Trim());
+
+                    foreach (string exp in exps1)
+                        lin1 += evalExp(exp.Trim(), var0, lin0);
+
+                    headers.Add (new HttpHeader("Cookie", lin1));
+                }
+            }
+            catch (Exception ex)
+            {
+                error = "HackSucuri.addSucuriHeaders -> " + ex.Message;
+            }
+
+            return (0 == error.Length);
+        }
+
+
+        //public static void passSucuri(string jsCrypt, ref Headers headers)
+        //{
+        //    Regex reg = new Regex("S='.+?'", RegexOptions.Singleline);
+
+        //    string lin0 = string.Empty;
+        //    string lin1 = string.Empty;
+
+        //    if (reg.IsMatch(jsCrypt))
+        //    {
+        //        string base64 = reg.Match(jsCrypt).Value.Split("'".ToCharArray()[0])[1];
+        //        string js = Encoding.UTF8.GetString(Convert.FromBase64String(base64));
+        //        string[] lines = getLines(js);
+
+        //        string var0 = lines[0].Split('=')[0];
+
+        //        string[] exps0 = lines[0].Substring(lines[0].IndexOf("=") + 1).Split('+');
+        //        string[] exps1 = lines[1].Substring(lines[1].IndexOf("=") + 1).Split('+');
+
+        //        foreach (string exp in exps0)
+        //            lin0 += evalExp(exp.Trim());
+
+        //        foreach (string exp in exps1)
+        //            lin1 += evalExp(exp.Trim(), var0, lin0);
+
+        //        headers.Add(new Header("Cookie", lin1));
+        //    }
+        //}
+
+        static string evalExp (string exp, string identifier = "", string change = "")
+        {
+            string val = string.Empty;
+
+            if (!exp.StartsWith("'") && !exp.StartsWith("\""))
+            {
+                if (exp.StartsWith("String.fromCharCode"))
+                {
+                    string value = exp.Split("()".ToCharArray())[1];
+
+                    val = value.StartsWith("0x") ?
+                        val = ((char)int.Parse(value.Substring(2), System.Globalization.NumberStyles.HexNumber)).ToString() :
+                        val = ((char)int.Parse(value)).ToString();
+                }
+                else
+                {
+                    if (exp == identifier)
+                        val = change;
+                    else
+                        throw new Exception("Comando desconocido");
+                }
+            }
+            else
+            {
+                string[] parts = exp.Split('.');
+
+                if (parts.Length > 1)
+                {
+                    string value = evalExp(parts[0]);
+                    string cmd = parts[1];
+
+                    if (cmd.StartsWith("charAt"))
+                    {
+                        int index = int.Parse(cmd.Split("()".ToCharArray())[1]);
+                        val = value.Substring(index, 1);
+                    }
+                    else if (cmd.StartsWith("substr"))
+                    {
+                        int index = int.Parse(cmd.Split("(,)".ToCharArray())[1]);
+                        int len = int.Parse(cmd.Split("(,)".ToCharArray())[2]);
+                        val = value.Substring(index, len);
+                    }
+                    else if (cmd.StartsWith("slice"))
+                    {
+                        int index = int.Parse(cmd.Split("(,)".ToCharArray())[1]);
+                        int len = int.Parse(cmd.Split("(,)".ToCharArray())[2]);
+                        val = value.Substring(index, len - index);
+                    }
+                    else
+                    {
+                        throw new Exception("Comando desconocido");
+                    }
+                }
+                else
+                {
+                    val = parts[0].Substring(1, parts[0].Length - 2);
+                }
+            }
+
+            return val;
+        }
+
+        static string[] getLines (string js)
+        {
+            List<string> lines = new List<string>();
+            string aux = string.Empty;
+            bool text = false;
+
+            for (int k = 0; k < js.Length; k++)
+            {
+                string c = js.Substring(k, 1);
+                bool i = (c == "'" || c == "(" || c == ")" || c == "\"");
+
+                if (i) text = !text;
+
+                if (c == ";")
+                {
+                    if (!text)
+                    {
+                        lines.Add(aux);
+                        aux = string.Empty;
+                    }
+                    else
+                    {
+                        aux += c;
+                    }
+                }
+                else
+                {
+                    aux += c;
+                }
+            }
+
+            return lines.ToArray();
+        }
+    }
+}
--- Scrapper/Commons/CustomHttpManager/HttpManager.cs
+++ Scrapper/Commons/CustomHttpManager/HttpManager.cs
@@ -1,12 +1,226 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Net;
+using System.IO;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Commons.CustomHttpManager
 {
-    class HttpManager
+    public enum Method
     {
+        POST,
+        GET,
+        HEAD
     }
+
+    public class HttpManager
+    {
+        public static bool request (string url, Method method, HttpHeaders headers, byte[] data, ref int codeResponse, ref string webResponse, ref string dataResponse, bool autoRedirect, ref string error)
+        {
+            codeResponse = -1;
+            webResponse = string.Empty;
+            dataResponse = string.Empty;
+
+            try
+            {
+                HttpWebRequest webRequest = (HttpWebRequest)WebRequest.Create(url);
+
+                webRequest.Method = (method == Method.GET ? "GET" : (method == Method.POST ? "POST" : "HEAD"));
+                webRequest.AllowAutoRedirect = autoRedirect;
+                webRequest.ReadWriteTimeout = 2000;
+
+                addHeaders (headers, ref webRequest);
+
+                webRequest.ContentLength = null != data ? data.Length : 0;
+
+                if (method == Method.POST)
+                {
+                    using (Stream st = webRequest.GetRequestStream())
+                    {
+                        st.Write(data, 0, data.Length);
+                        st.Close();
+                    }
+                }
+
+                using (WebResponse wres = webRequest.GetResponse())
+                {
+                    HttpWebResponse httpws = (HttpWebResponse)wres;
+
+                    codeResponse = (int)httpws.StatusCode;
+                    webResponse = httpws.StatusDescription;
+
+                    if (autoRedirect)
+                    {
+                        if (httpws.StatusCode != HttpStatusCode.OK)
+                            throw new Exception(string.Format("Server error (HTTP {0}: {1})", (int)httpws.StatusCode, httpws.StatusDescription));
+                    }
+                    else
+                    {
+                        if (httpws.StatusCode != HttpStatusCode.OK && httpws.StatusCode != (HttpStatusCode)302)
+                            throw new Exception(string.Format("Server error (HTTP {0}: {1})", (int)httpws.StatusCode, httpws.StatusDescription));
+                    }
+
+                    using (StreamReader sr = new StreamReader(wres.GetResponseStream()))
+                        dataResponse = sr.ReadToEnd();
+
+                    if (dataResponse.Length == 0)
+                    {
+                        if (!autoRedirect && codeResponse == 302)
+                        {
+                            if (httpws.Headers.Count > 0)
+                                if (httpws.Headers["Location"] != null)
+                                    dataResponse = httpws.Headers["Location"].ToString();
+                        }
+                        else
+                        {
+                            if (method == Method.HEAD)
+                            {
+                                dataResponse = httpws.Headers.ToString();
+                            }
+                        }
+
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                error = "HttpHelper.request -> " + ex.Message;
+            }
+
+            return (0 == error.Length);
+        }
+
+
+
+
+
+        static void addHeaders (HttpHeaders headers, ref HttpWebRequest webRequest)
+        {
+            if (null != headers)
+            {
+                foreach (HttpHeader h in headers)
+                {
+                    bool existe = false;
+
+                    foreach (HttpRequestHeader e in Enum.GetValues(typeof(HttpRequestHeader)))
+                    {
+                        if (e.ToString() == h.key)
+                        {
+                            switch (e)
+                            {
+                                case HttpRequestHeader.Referer:
+
+                                    webRequest.Referer = h.value;
+                                    existe = true;
+                                    break;
+
+                                case HttpRequestHeader.ContentType:
+
+                                    webRequest.ContentType = h.value;
+                                    existe = true;
+                                    break;
+
+                                case HttpRequestHeader.UserAgent:
+
+                                    webRequest.UserAgent = h.value;
+                                    existe = true;
+                                    break;
+
+                                case HttpRequestHeader.Cookie:
+
+                                    string[] cookies = h.value.Split(';');
+
+                                    webRequest.CookieContainer = new CookieContainer(cookies.Length);
+
+                                    foreach (string cookie in cookies)
+                                    {
+                                        string name = cookie.Split('=')[0];
+                                        string value = cookie.Split('=')[1];
+
+                                        if (value == "/") value = "";
+
+                                        webRequest.CookieContainer.Add(new Cookie(name, value, "", webRequest.RequestUri.Host));
+                                    }
+
+                                    existe = true;
+                                    break;
+                            }
+
+                            if (existe)
+                                break;
+                        }
+                    }
+
+                    if (!existe)
+                        webRequest.Headers.Add(h.key, h.value);
+                }
+            }
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        public static bool requestGet (string url, HttpHeaders headers, ref string dataResponse, ref string error)
+        {
+            int     codeResponse = 0;
+            string  webResponse  = string.Empty;
+
+            return request (url, Method.GET, headers, null, ref codeResponse, ref webResponse, ref dataResponse, true, ref error);
+        }
+
+        public static bool requestGetSinReditect(string url, HttpHeaders headers, ref string dataResponse, ref string error)
+        {
+            int codeResponse = 0;
+            string webResponse = string.Empty;
+
+            return request(url, Method.GET, headers, null, ref codeResponse, ref webResponse, ref dataResponse, false, ref error);
+        }
+
+        public static bool requestPost (string url, HttpHeaders headers, string data, ref string dataResponse, ref string error)
+        {
+            int    codeResponse = 0;
+            string webResponse  = string.Empty;
+
+            return request(url, Method.POST, headers, Encoding.UTF8.GetBytes(data), ref codeResponse, ref webResponse, ref dataResponse, true, ref error);
+        }
+
+        public static bool requestHead (string url, HttpHeaders headers, ref string dataResponse, ref string error)
+        {
+            int    codeResponse = 0;
+            string webResponse  = string.Empty;
+
+            return request(url, Method.HEAD, headers, null, ref codeResponse, ref webResponse, ref dataResponse, false, ref error);
+        }
+
+        
+    }
+
 }
